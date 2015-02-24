3eee1090665560f595900dd9221a7940cdcdf1ab
Palmér <spr10002@student.mdh.se>
2015-02-24 17:28:25 +0100

    Added BlessYou SLN

---
+++ Program/BlessYou/BlessYou/Case.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlessYou
+{
+    class Case
+    {
+        string _filePath;
+        bool _sneazy;
+        List<List<double>> _featureVectors;
+    }
+}
---
+++ Program/BlessYou/BlessYou/FeatureExtractor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlessYou
+{
+    class FeatureExtractor
+    {
+
+        void _extractWavFileFeatures(string path)
+        { }
+
+        void _loadFeatureList(string path)
+        { }
+
+    }
+}
