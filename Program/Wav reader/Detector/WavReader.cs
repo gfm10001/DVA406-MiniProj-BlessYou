bb4ef7f5ad89198c22759b3331de423957d630af
Palmér <spr10002@student.mdh.se>
2015-02-25 12:30:44 +0100

    Work on Wavextractor
    Header extractor now working

--- Program/Wav reader/Detector/WavReader.cs
+++ Program/Wav reader/Detector/WavReader.cs
@@ -4,20 +4,22 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.InteropServices;
 
 namespace Detector
 {
     class WavReader
     {
-        struct fileheader
+         unsafe struct fileheader
         {
-            public string sGroupID;       //Surprisingly enough, this is always "RIFF"
+            public fixed char sGroupID[4];       //Surprisingly enough, this is always "RIFF"
             public uint dwFileLength;   //File length in bytes, measured from offset 8
-            public string sRiffType;      //In wave files, this is always "WAVE"
+            public fixed char sRiffType[4];      //In wave files, this is always "WAVE"
         }
-        struct fmtchunk
+        unsafe struct fmtchunk
         {
-            public string sChunkID;        //Four bytes: "fmt "
+            public fixed char sChunkID[4];        //Four bytes: "fmt "
             public uint dwChunkSize;     //Length of header in bytes
             public ushort wFormatTag;      //1 if uncompressed Microsoft PCM audio
             public ushort wChannels;       //Number of channels
@@ -26,13 +28,13 @@ namespace Detector
             public ushort wBlockAlign;     //Sample frame size in bytes
             public uint dwBitsPerSample; //Bits per sample
         }
-        struct datachunk
+        unsafe struct datachunk
         {
-            public string sChunkID;       //Four bytes: "data"
+            public fixed char sChunkID[4];       //Four bytes: "data"
             public uint dwChunkSize;    //Length of header in bytes
             //Different arrays for the different frame sizes
-            public byte[] byteArray;     //8 bit unsigned data; or...
-            public short[] shortArray;    //16 bit signed data
+            //public fixed byte byteArray[8];     //8 bit unsigned data; or...
+            public fixed short shortArray[16];    //16 bit signed data
         }
 
         static string GetString(byte[] bytes)
@@ -45,21 +47,71 @@ namespace Detector
         byte[] data;
         fileheader header;
         fmtchunk fmt;
+        
 
 
         public WavReader()
         { }
-        public void ReadFile(string filepath)
+        unsafe public void ReadFile(string filepath)
         {
-            data = File.ReadAllBytes(filepath);
-            //BinaryReader br = new BinaryReader(File.Open(filepath, FileMode.Open));
+            data = File.ReadAllBytes(filepath); //Load file into memory
+            fixed (fileheader* pheader = &header) //Extract file header
+            {
 
-            header.sGroupID = Encoding.Default.GetString(data,0,4);
-            header.dwFileLength = BitConverter.ToUInt32(data, 4);
-            //header.sRiffType = BitConverter.ToString(
+                char[] temp = new char[4];
+                temp = Encoding.Default.GetString(data, 0, 4).ToCharArray();
+                for(int i=0; i<4;i++)
+                    pheader->sGroupID[i] = temp[i];
+                
+                header.dwFileLength = BitConverter.ToUInt32(data, 4);
+               
+                temp = Encoding.Default.GetString(data, 8, 4).ToCharArray();
+                for (int i = 0; i < 4; i++)
+                    pheader->sRiffType[i] = temp[i];
+            }
+            fixed (fmtchunk* pfmt = &fmt) //extract fmt header
+            {
+                char[] temp = Encoding.Default.GetString(data, 12, 4).ToCharArray();
+                for (int i = 0; i < 4; i++)
+                    pfmt->sChunkID[i] = temp[i];
 
-            header.sRiffType = Encoding.Default.GetString(data,8,4);        
+                fmt.dwChunkSize = BitConverter.ToUInt32(data, 16);
+                fmt.wFormatTag = BitConverter.ToUInt16(data, 20);
+                fmt.wChannels = BitConverter.ToUInt16(data, 22);
+                fmt.dwSamplesPerSec = BitConverter.ToUInt32(data, 24);
+                fmt.dwAvgBytesPerSec = BitConverter.ToUInt32(data, 28);
+                fmt.wBlockAlign = BitConverter.ToUInt16(data, 32);
+                fmt.dwBitsPerSample = BitConverter.ToUInt16(data, 34);
+            }
+
+            if (header.dwFileLength > Int32.MaxValue) 
+                throw new InvalidDataException("File too big to be analyzed!");
+
+                int pointer = 36;
+                int limit = data.Length;
+                datachunk sound;
+                BinaryFormatter formatter = new BinaryFormatter();
+                MemoryStream ms = new MemoryStream(data);
+                while (pointer < limit) //extract data
+                {
+                    string chunkid = Encoding.Default.GetString(data, pointer, 4);
+                    string temp = Encoding.Default.GetString(data, 0, data.Length);
+                    if (chunkid == "data")
+                    {
+                        datachunk chunk = new datachunk();
+                        
+                    
+                    }
+                
+                
+                }
+                //formatter.Deserialize(ms);
+
+                //MemoryStream mstream = new MemoryStream();
+                //BinaryFormatter formatter = new BinaryFormatter();
+                //mstream.Write(data, 0, data.Length);
+                //mstream.Seek(16, SeekOrigin.Begin);
+                //fmt = (fmtchunk)formatter.Deserialize(mstream);
         }
-
     }
 }
