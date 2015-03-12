520001503b7759e93a92e5b167a2726cb1a3b9bd
Göran <gfm10001@student.mdh.se>
2015-03-12 14:53:26 +0100

    Demo Prep

---
+++ Program/BlessYou/run-all.bat
@@ -0,0 +1,3 @@
+cd BlessYou\bin\Debug\
+start "All" BlessYou.exe ..\..\..\samplesFileNames-all.txt all
+cd ..\..\..
---
+++ Program/BlessYou/run-demo-mix-75s-25ns.bat
@@ -0,0 +1,3 @@
+cd BlessYou\bin\Debug\
+start "Mix 75svs 25ns" BlessYou.exe ..\..\..\samplesFileNames-demo-mix-75s-25ns.txt all
+
