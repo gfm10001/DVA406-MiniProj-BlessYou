7dbd2d80db364cc8417860fea6c11bd7bfd6c369
Göran <gfm10001@student.mdh.se>
2015-03-17 11:19:47 +0100

    Added bat file for interactive case

---
+++ Program/BlessYou/run-all + 1.bat
@@ -0,0 +1,5 @@
+cd BlessYou\bin\Debug\
+start "All + 1" BlessYou.exe ..\..\..\samplesFileNames-all.txt ..\..\..\..\Data\NewProblems\Sneeze-from-Youtube.wav
+cd ..\..\..
+
+Rem ..\..\..\..\Data\NewProblems\Explosion.wav
