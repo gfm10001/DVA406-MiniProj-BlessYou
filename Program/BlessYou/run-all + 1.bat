8ffa1aefb8a9c6eb0f86d8e4e5a5c8e65a06b3fa
Göran <gfm10001@student.mdh.se>
2015-03-17 13:43:00 +0100

    Added teacher sounds

---
+++ Program/BlessYou/run-all + 1.bat
@@ -0,0 +1,9 @@
+cd BlessYou\bin\Debug\
+start "All + 1" BlessYou.exe ..\..\..\samplesFileNames-all.txt ..\..\..\..\Data\NewProblems\Sneeze-from-Youtube.wav
+cd ..\..\..
+
+exit
+..\..\..\..\Data\NewProblems\Explosion.wav
+..\..\..\..\Data\NewProblems\Teacher_1A.wav
+..\..\..\..\Data\NewProblems\Teacher_1B.wav
+..\..\..\..\Data\NewProblems\Teacher_2.wav
